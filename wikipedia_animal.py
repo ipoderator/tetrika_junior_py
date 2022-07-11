d2a8f42f6b8f88642c4a1f400ecf4349e9eef175
Gleb Churkin <gleb.churkin48@gmail.com>
2022-07-11 18:09:10 +0300

    Add except KeyboardInterrupt

--- wikipedia_animal.py
+++ wikipedia_animal.py
@@ -17,27 +17,33 @@ RUSSIAN_DICT = list('АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
 
 """ Функция вывод все страницы из категории животные """
 def main():
-    # Выводим все члены данной категории 
-    all_animals = ANIMAL_PAGES.categorymembers 
-    """ 
-    Цикл который проходится по буквам алфавита и животными.
-    Далее заносит данный в словарь. Где ключь - это буква алфавита,
-    а значение - это кол-во животных которые принадлежат данной группе
-    """
-    for animals_dict in RUSSIAN_DICT:
-        animals_count = get_all_animals(all_animals, animals_dict)
-        print(f"{animals_dict}: {animals_count}")
+    try:
+        # Выводим все члены данной категории 
+        all_animals = ANIMAL_PAGES.categorymembers 
+        """ 
+        Цикл который проходится по буквам алфавита и животными.
+        Далее заносит данный в словарь. Где ключь - это буква алфавита,
+        а значение - это кол-во животных которые принадлежат данной группе
+        """
+        for animals_dict in RUSSIAN_DICT:
+            animals_count = get_all_animals(all_animals, animals_dict)
+            print(f"{animals_dict}: {animals_count}")
+    except KeyboardInterrupt:
+        print("Ошибка, что-то не так")
 
 
 """ Функция проходит по всем животным и подсчитывает их кол-во по title """
 logger.info("Established a connection")
 def get_all_animals(all_animals, animals_dict):
-    counter = 0
-    for animal in all_animals.values():
-        if animal.title[:1] == animals_dict:
-            counter += 1
-    return counter
-    
+    try:
+        counter = 0
+        for animal in all_animals.values():
+            if animal.title[:1] == animals_dict:
+                counter += 1
+        return counter
+    except KeyboardInterrupt:
+        print("Ошибка, что-то не так")        
+
 
 if __name__ == "__main__":
     t0 = time() # Засикаем время    
